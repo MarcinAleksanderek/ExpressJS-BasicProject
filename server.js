598ca32d1a32865e4cfa0b975803ac1568ac7d06
Marcin <marcin.aleksanderek@gmail.com>
2019-06-20 11:31:30 +0200

    Add data transfer between endpoints

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 var express = require('express');
-bodyParser = require('body-parser');
-var fs = require('fs')
 var app = express();
-var stringifyFile;
+app.use(express.static('assets'));
 
-app.use(bodyParser.json());
-
-app.use('/', function (req, res, next) {
-	fs.readFile('./test.json', 'utf8', function (err, data) {
-		if (err) throw err;
-		stringifyFile = data;
-		next();
-	})
-})
-
-app.get('/getNote', function (req, res) {
-	res.send(stringifyFile);
+app.get('/', function (req, res) {
+	//u mnie z automatu serwuje plik index.html bo udostepnia folder assets. 
+	//nie da sie w takim razie zdefiniowac defaulowego zachowania dla ogólnego adresu, jedyna opcja to zmienić nazwe index.html na inną?
+	res.send('Hello world');
+	//res.sendFile('/index.html')
 });
 
-app.post('/updateNote/:note', function (req, res) {
-	stringifyFile += req.params.note;
-	fs.writeFile('./test.json', stringifyFile, function (err) {
-		if (err) throw err;
-		res.send(stringifyFile);
-	})
+app.get('/userform', function (req, res) {
+	const response = {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name
+	};
+	res.json(response);
 });
 
-app.listen(3000);
+
+var server = app.listen(3000, 'localhost', function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
